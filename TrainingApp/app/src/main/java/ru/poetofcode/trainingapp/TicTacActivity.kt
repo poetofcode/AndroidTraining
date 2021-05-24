a24a9722a84a822eb5358fdbfd720e32ea0e9b5d
poetofcode <poetinthecode@gmail.com>
2021-05-24 18:45:28 +0300

    Added project files

---
+++ TrainingApp/app/src/main/java/ru/poetofcode/trainingapp/TicTacActivity.kt
@@ -0,0 +1,11 @@
+package ru.poetofcode.trainingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TicTacActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tic_tac)
+    }
+}
