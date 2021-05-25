ba7962f4bda803b79076aecddf66f4790b890108
poetofcode <poetinthecode@gmail.com>
2021-05-25 16:33:34 +0300

    Added sample with proxy (converted) observable

--- TrainingApp/rx_samples/src/main/java/ru/poetofcode/rx_samples/Launcher.java
+++ TrainingApp/rx_samples/src/main/java/ru/poetofcode/rx_samples/Launcher.java
@@ -4,17 +4,23 @@ import java.util.Arrays;
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.observables.ConnectableObservable;
 
 public class Launcher {
 
     private static int currentSample = 1;
 
+    private static ObservableEmitter<String> convertedEmitter;
+
     public static void main(String[] args) {
 
         sampleOne_Errors();
 
         sampleTwo_Creation();
+
+        sampleThree_ProxyObservable();
 
         log("\nFINISH APP");
     }
@@ -96,6 +102,28 @@ public class Launcher {
         helloHot.subscribe(System.out::print, e -> {}, () -> log(""));
 
         helloHot.connect();
+    }
+
+    private static void sampleThree_ProxyObservable() {
+        logSampleHeader();
+
+        Observable<String> strings = Observable.just("Red", "Blue", "White");
+
+        convertObservable(strings).subscribe(Launcher::log).dispose();
+    }
+
+    private static Observable<String> convertObservable(Observable<String> source) {
+        Observable<String> res = Observable.create(emitter -> {
+            Launcher.convertedEmitter = emitter;
+
+            Disposable d = source.subscribe(str -> {
+                Launcher.convertedEmitter.onNext("converted-" + str);
+            }, Throwable::printStackTrace, () -> Launcher.convertedEmitter.onComplete());
+
+            emitter.setDisposable(d);
+        });
+
+        return res;
     }
 
 }
