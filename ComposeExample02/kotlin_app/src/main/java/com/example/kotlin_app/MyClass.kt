366370d6ad43a4fdb673468632ba52087e237995
poetofcode <poetinthecode@gmail.com>
2021-08-12 22:48:29 +0300

    MyClass updated

--- ComposeExample02/kotlin_app/src/main/java/com/example/kotlin_app/MyClass.kt
+++ ComposeExample02/kotlin_app/src/main/java/com/example/kotlin_app/MyClass.kt
@@ -1,10 +1,7 @@
 package com.example.kotlin_app
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
 
 @DelicateCoroutinesApi
 @InternalCoroutinesApi
@@ -16,9 +13,16 @@ fun main(args: Array<String>) {
 
 @InternalCoroutinesApi
 suspend fun testFlow() {
-    val input = 1..10
-    input.asFlow()
-        .groupToList { it % 2 }
+    val pagingArrOfProductItems = listOf(
+        PagingData(ProductItem("Product 1")),
+        PagingData(ProductItem("Product 2")),
+        PagingData(ProductItem("Product 3")),
+        PagingData(ProductItem("Product 4")),
+        PagingData(ProductItem("Product 5"))
+
+    )
+    pagingArrOfProductItems.asFlow()
+        .groupByPair()
         .collect { println(it) }
 }
 
@@ -30,3 +34,32 @@ fun <T, K> Flow<T>.groupToList(getKey: (T) -> K): Flow<Pair<K, List<T>>> = flow 
     }
     storage.forEach { (k, ts) -> emit(k to ts) }
 }
+
+
+@InternalCoroutinesApi
+fun Flow<PagingData<ProductItem>>.groupByPair(): Flow<PagingData<ProductRow>> = flow {
+    val storage = mutableListOf<PagingData<ProductRow>>()
+    val tempPair = mutableListOf<ProductItem>()
+    val addToStorage: (MutableList<ProductItem>) -> Unit = {
+        storage += PagingData(ProductRow(it.toList()))
+        it.clear()
+    }
+    collect { item ->
+        tempPair += item.value
+        if (tempPair.size < 2) return@collect
+        addToStorage(tempPair)
+    }
+    if (tempPair.isNotEmpty()) addToStorage(tempPair)
+    storage.forEach { emit(it) }
+}
+
+
+//
+// Data classes
+//
+
+data class PagingData<T>(val value: T)
+
+data class ProductItem(val productName: String)
+
+data class ProductRow(val items: List<ProductItem>)
