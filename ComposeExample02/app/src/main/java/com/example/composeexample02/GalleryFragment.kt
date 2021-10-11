a191a04f1a9b00d593525d6cb582110dc6e70631
poetofcode <poetinthecode@gmail.com>
2021-10-11 22:49:48 +0300

    Looping pager

--- ComposeExample02/app/src/main/java/com/example/composeexample02/GalleryFragment.kt
+++ ComposeExample02/app/src/main/java/com/example/composeexample02/GalleryFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -44,7 +45,8 @@ class GalleryFragment : Fragment() {
     @Composable
     fun Root() {
         val images = FakeData.IMAGES
-        val pagerState = rememberPagerState()
+        val startIndex = Int.MAX_VALUE / 2
+        val pagerState = rememberPagerState(initialPage = startIndex)
 
         val current = pagerState.currentPage
         val target = pagerState.targetPage ?: current
@@ -54,15 +56,18 @@ class GalleryFragment : Fragment() {
                 .background(Color.Black)
                 .fillMaxSize()) {
             HorizontalPager(
-                count = images.size,
+                count = Int.MAX_VALUE,
                 state = pagerState,
                 modifier = Modifier.fillMaxSize(),
-                itemSpacing = 0.dp
+                itemSpacing = 0.dp,
+                contentPadding = PaddingValues(end = 64.dp),
             ) { pageIdx ->
+
+                val page = (pageIdx - startIndex).floorMod(images.size)
 
                 GlideImage(
                     modifier = Modifier.fillMaxSize(),
-                    request = images[pageIdx],
+                    request = images[page],
                     contentScale = ContentScale.Fit
                 )
 
@@ -70,5 +75,9 @@ class GalleryFragment : Fragment() {
         }
     }
 
+    private fun Int.floorMod(other: Int): Int = when (other) {
+        0 -> this
+        else -> this - floorDiv(other) * other
+    }
 
 }
