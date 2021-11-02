b3deb1eb000097b1724463b8719dd10204d8486c
poetofcode <poetinthecode@gmail.com>
2021-11-02 23:59:20 +0300

    Added button handler

--- AnimationExample/app/src/main/java/com/example/animationexample/ui/main/MainViewModel.kt
+++ AnimationExample/app/src/main/java/com/example/animationexample/ui/main/MainViewModel.kt
@@ -1,9 +1,16 @@
 package com.example.animationexample.ui.main
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
     val messageText: String = "String from Binding!"
+
+    fun onAnimateButtonClick(v: View) {
+
+        println("mylog On Animate Click !!!")
+
+    }
 
 }
