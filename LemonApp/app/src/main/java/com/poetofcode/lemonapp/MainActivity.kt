23efb89cf811b62d00111e9d4fff73b0aabf9042
poetofcode <cyber-post@yandex.ru>
2022-06-25 03:43:27 +0300

    LemonApp: added main-menu screen

--- LemonApp/app/src/main/java/com/poetofcode/lemonapp/MainActivity.kt
+++ LemonApp/app/src/main/java/com/poetofcode/lemonapp/MainActivity.kt
@@ -10,31 +10,27 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.poetofcode.lemonapp.screen.MenuScreen
 import com.poetofcode.lemonapp.ui.theme.LemonAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LemonAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            AppEntryPoint()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LemonAppTheme {
-        Greeting("Android")
+fun AppEntryPoint() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "menu") {
+        composable(route = "menu") {
+            MenuScreen()
+        }
     }
 }
