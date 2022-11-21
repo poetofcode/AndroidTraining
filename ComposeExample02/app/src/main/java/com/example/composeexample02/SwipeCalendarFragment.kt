c8d2a7b3b0ce031ba6bf4823226aad68161cb828
poetofcode <cyber-post@yandex.ru>
2022-11-21 12:13:03 +0300

    Added CoroutineLearningApp; SwipeCalendar component: in progress;

---
+++ ComposeExample02/app/src/main/java/com/example/composeexample02/SwipeCalendarFragment.kt
@@ -0,0 +1,31 @@
+package com.example.composeexample02
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class SwipeCalendarFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Root()
+            }
+        }
+    }
+
+    @Composable
+    private fun Root() {
+        Text(text = "SwipeCalendar fragment")
+    }
+
+}
