0246d5e7903bedb8ab485973aa2f44de5c758798
poetofcode <cyber-post@yandex.ru>
2023-03-20 11:03:20 +0300

    Initial commint

--- FigureApp/app/src/main/java/com/example/figureapp/MainActivity.kt
+++ FigureApp/app/src/main/java/com/example/figureapp/MainActivity.kt
@@ -3,11 +3,10 @@ package com.example.figureapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,7 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.figureapp.ui.theme.FigureAppTheme
 
@@ -42,9 +41,9 @@ class MainActivity : ComponentActivity() {
                         )
                         Text(text = "Окружность")
                     }
-                    
+
                     Spacer(modifier = Modifier.size(50.dp))
-                    
+
                     Figures(isQuad = isQuad.value)
                 }
             }
@@ -54,8 +53,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Figures(isQuad: Boolean) {
-    Row {
-
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 20.dp),
+        horizontalArrangement = Arrangement.spacedBy(20.dp)) {
+        listOf(Color.Blue, Color.Yellow, Color.Green).forEach {
+            Circle().Draw(color = it)
+        }
     }
 }
 
+abstract class Figure {
+
+    @Composable
+    abstract fun Draw(color: Color)
+}
+
+class Rectangle : Figure() {
+
+    @Composable
+    override fun Draw(color: Color) {
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(color))
+    }
+}
+
+class Circle : Figure() {
+
+    @Composable
+    override fun Draw(color: Color) {
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(color = color, shape = CircleShape))
+    }
+}
