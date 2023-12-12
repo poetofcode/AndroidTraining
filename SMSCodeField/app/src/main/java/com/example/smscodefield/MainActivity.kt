afaa2f536930161a508b0615f2188ddc85fb584c
poetofcode <cyber-post@yandex.ru>
2023-12-12 12:36:38 +0300

    Added SMSCodeField

---
+++ SMSCodeField/app/src/main/java/com/example/smscodefield/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.smscodefield
+
+import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val areas = listOf(R.id.tvArea1, R.id.tvArea2, R.id.tvArea3, R.id.tvArea4).map {
+//            findViewById<TextView>(it)
+//        }
+//
+//        val fieldCode = findViewById<EditText>(R.id.etCode)
+//        areas.forEach {
+//            it.setOnClickListener {
+//                fieldCode.requestFocus()
+//                println("mylog ${"Click on area"}")
+//
+//                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+//                imm.showSoftInput(fieldCode, InputMethodManager.SHOW_IMPLICIT)
+//            }
+//        }
+    }
+
+}
