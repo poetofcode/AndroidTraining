10b92692253fd39d0951ede8d9516a991bc345da
poetofcode <cyber-post@yandex.ru>
2024-02-14 15:08:41 +0300

    sie2api sample: initial commit

---
+++ site2apisample/app/src/main/java/com/poetofcode/site2api_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.poetofcode.site2api_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
