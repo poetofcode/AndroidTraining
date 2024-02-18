2b77340e8d5556a5071d8cb3a39059d876eabc33
poetofcode <cyber-post@yandex.ru>
2024-02-18 22:34:33 +0300

    site2api-sample: added compose

--- site2apisample/app/src/main/java/com/poetofcode/site2api_sample/MainActivity.kt
+++ site2apisample/app/src/main/java/com/poetofcode/site2api_sample/MainActivity.kt
@@ -2,8 +2,17 @@ package com.poetofcode.site2api_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.poetofcode.site2api_sample.ui.theme.Site2apisampleTheme
 import java.util.Properties
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +21,41 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
 
-        val baseUrl = resources.getString(R.string.backendBaseUrl)
-        val apiKey = resources.getString(R.string.apiKey)
-        vm.initAPI(baseUrl, apiKey)
-        vm.loadFeed()
+        setContent {
+            Site2apisampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+
+
+//        val baseUrl = resources.getString(R.string.backendBaseUrl)
+//        val apiKey = resources.getString(R.string.apiKey)
+//        vm.initAPI(baseUrl, apiKey)
+//        vm.loadFeed()
+    }
+
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        Site2apisampleTheme {
+            Greeting("Android")
+        }
     }
 }
